060be0d32d140fcba80a36f7f71c7cc8df09f8d0
Matthew <78285385+matthew55@users.noreply.github.com>
2026-04-01 20:04:14 -0400

    Add git to pre-installs

--- .chezmoiscripts/run_once_before_00_install-password-manager.sh
+++ .chezmoiscripts/run_once_before_00_install-password-manager.sh
@@ -4,5 +4,5 @@
 (type age >/dev/null 2>&1 && type keepassxc-cli >/dev/null 2>&1) && exit
 
 echo "Installing required dependencies for chezmoi setup..."
-sudo pacman -S --needed --noconfirm age keepassxc
+sudo pacman -S --needed --noconfirm age git keepassxc
 echo "Done!"
